2d43a2c3af1dcff4eb59252d80c26ba6e94a4254
Victor Faria <victor.faria@standardbank.co.mz>
2022-10-27 22:40:34 +0200

    My First App

---
+++ myfirstapp/src/main/java/com/example/demo/Greetings.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class Greetings {
+	
+	private final long id;
+	private final String content;
+	
+	public Greetings(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	
+}
---
+++ myfirstapp/src/main/java/com/example/demo/GreetingsController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+	
+
+	@RestController
+	public class GreetingsController {
+
+	    private static final String template = "Hello, %s";
+	    private final AtomicLong counter = new AtomicLong();
+	    
+	    @RequestMapping("/greetings")
+	    public Greetings greeting(@RequestParam(value ="name", defaultValue= "Word") String name) 
+	    {
+	
+		return new Greetings(counter.incrementAndGet(), String.format(template, name));
+	    }
+
+}
